d1e8c3e37c892f495cee634438add35dcd4b6d1d
danilo775 <danilo.uba@hotmail.com>
2025-08-20 23:16:34 -0300

    database seed update

---
+++ src/main/java/com/devsuperior/dslist/entities/belonging.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Table(name = "tb_belonging")
+@Entity
+public class belonging {
+	
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	
+	private Long position;
+	
+	public belonging() {
+		
+	}
+	
+	public belonging(Game game, GameList list, Long position) {
+		id.setGame(game);
+		id.setList(list);
+		this.position = position;
+	}
+	
+	public BelongingPK getId() {
+		return id;
+	}
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+	public Long getPosition() {
+		return position;
+	}
+	public void setPosition(Long position) {
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		belonging other = (belonging) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
